f5575f3ea957980bccca4468dc2939621d125148
Mz7414 <95566213+Mz7414@users.noreply.github.com>
2023-04-27 18:30:53 +0800

    Update MangaAndNovel.py

--- MangaAndNovel.py
+++ MangaAndNovel.py
@@ -22,22 +22,17 @@ def mange(*args):
     for arg in args :
         url = f"https://www.manhuagui.com/comic/{arg}/"
         resp = requests.get(url)
-        data={
-          'message':
-            resp.text
-        }
-        line(data)
         soup = BeautifulSoup(resp.text,"html.parser")
         title = soup.select("div.book-title > h1")[0].text
         time = soup.select("span > span:nth-child(3)")[0].text #更新時間
         x = soup.select("li.status > span > a")[0].text  #最新話數
-#         if time == y :
-#             data = {
-#                 'message': 
-#                 "\n"+
-#                 f"漫畫:《{title}》已更新至{x}"
-#             }
-#             line(data)
+        if time == y :
+            data = {
+                'message': 
+                "\n"+
+                f"漫畫:《{title}》已更新至{x}"
+            }
+            line(data)
             
 def novel(*args):
     for arg in args : 
@@ -64,7 +59,6 @@ def novel(*args):
 #             line(data)
          
 
-#mange(34439,7580,6414,5173,36152,1676,28356,17473)        #看漫畫
+mange(34439,7580,6414,5173,36152,1676,28356,17473)        #看漫畫
 #novel(2356,2059,2139,6,3181,9,2727,3286,8,2513,3161,3095) #逼哩輕小說 
 novel(6)
-mange(1676)
